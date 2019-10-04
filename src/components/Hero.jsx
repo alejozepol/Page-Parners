4a649147d8134029c23148955efabf99b13c7b95
Alejandro Lopez <alejozepol@gmail.com>
2019-10-04 17:04:13 -0500

    actualziacion de colores hover btn

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,24 +3,27 @@ import BgHero from '../assets/static/hero510.png';
 import '../assets/styles/components/Hero.scss';
 import '../assets/styles/components/Button.scss';
 
-const Hero = () => (
-  <section className='Hero'>
-    <img src={BgHero} alt='imagen deportiva' />
-    <div className='Hero__title'>
-      <h2>Entrenemos Juntos</h2>
-    </div>
-    <div className='Hero__subTitle'>
-      <h3>Descubre</h3>
-      <p>-</p>
-      <h3>Conecta</h3>
-      <p>-</p>
-      <h3>Practica</h3>
-    </div>
-    <div className='Hero__button'>
-      <button type='button' className='btn btn-blanco'>Demo</button>
-      <button type='button' className='btn btn-blanco-out'>Descubre Mas</button>
-    </div>
-  </section>
-);
+const Hero = () => {
+  const urlDemo = 'http://parners.co/';
+  return (
+    <section className='Hero'>
+      <img src={BgHero} alt='imagen deportiva' />
+      <div className='Hero__title'>
+        <h2>Entrenemos Juntos</h2>
+      </div>
+      <div className='Hero__subTitle'>
+        <h3>Descubre</h3>
+        <p>-</p>
+        <h3>Conecta</h3>
+        <p>-</p>
+        <h3>Practica</h3>
+      </div>
+      <div className='Hero__button'>
+        <a type='button' href='x' className='btn btn-blanco'>Descubre Más</a>
+        <a type='button' href={urlDemo} className='btn btn-blanco-out'>Ver Demostracion</a>
+      </div>
+    </section>
+  );
+};
 
 export default Hero;
