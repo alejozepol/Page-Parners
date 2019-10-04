b590ba25f5a82d7bed07ffc3594c5872a06a8aa2
Alejandro Lopez <alejozepol@gmail.com>
2019-10-04 12:43:30 -0500

    cambio a clase button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Button = () =>(
-    
-)
+class Button extends Component {
+
+    handleclick = () =>{
+      this.setState({
+        count : this.state.count+ 1,
+      })
+    }
+
+    render() {
+      const { count } = this.state
+      return(
+        <div>
+          <h1>Manzanas: </h1>
+          <button type="button" onClick={this.handleclick}>
+                    Agregar
+          </button>
+        </div>
+      )
+    }
+
+}
 
 export default Button;
