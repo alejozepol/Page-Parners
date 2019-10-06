162cbc8ee88dde6ec84b67eee8acb75b004c88dc
Alejandro Lopez <alejozepol@gmail.com>
2019-10-05 20:06:29 -0500

    cambio tamaño hero y cambio color texto textblock

--- src/components/TextBlock.jsx
+++ src/components/TextBlock.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import '../assets/styles/components/TextBlock.scss';
 
 const TextBlock = ({ title, content }) => (
   <section className='textBlock'>
     <div className='textBlock__title'>
       <h3>{ title }</h3>
     </div>
-    <div>
+    <div className='textBlock__text'>
       <p>{ content }</p>
     </div>
   </section>
